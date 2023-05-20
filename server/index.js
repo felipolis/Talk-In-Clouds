8b0b7c4e443a51de13d9d14578582c4765f0621d
Felipe Archanjo <felipecunhamendes@gmail.com>
2023-05-20 19:18:50 -0300

    Inicializando o servidor e conectando ao banco de dados

---
+++ server/index.js
@@ -0,0 +1,38 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import http from 'http';
+import mongoose from 'mongoose';
+import "dotenv/config";
+import colors from 'colors';
+
+// -------- CONFIGURAÇÕES --------
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+const port = process.env.PORT || 5000;
+const server = http.createServer(app);
+
+
+// -------- INICIALIZAÇÃO DO SERVIDOR HTTP E BANCO DE DADOS --------
+
+const startServer = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log(`MongoDB connected`.cyan.underline.bold);
+        server.listen(port, () => console.log(`Server running on port ${port}`.yellow.bold));
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+startServer();
+
+// -------- SOCKETS --------
