f05be4d44f6b73d97843ef490d71c574170c69d7
Felipe Archanjo <felipecunhamendes@gmail.com>
2023-05-21 11:17:22 -0300

    Criação do middleware

---
+++ server/src/middlewares/token.middleware.js
@@ -0,0 +1,22 @@
+import jsonwebtoken from "jsonwebtoken";
+import responseHandler from "../handlers/response.handler.js";
+import userModel from "../models/user.model.js";
+
+const tokenDecode = (req) => {
+    try {
+        const bearerHeader = req.headers['authorization'];
+
+        if (bearerHeader) {
+            const token = bearerHeader.split(' ')[1];
+
+            return jsonwebtoken.verify(
+                token,
+                process.env.TOKEN_SECRET_KEY
+            )
+        }
+
+        return false
+    } catch {
+        return false
+    }
+}
