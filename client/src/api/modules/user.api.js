2526e237b6808f9dd39e516f28d27f3780696d62
Felipe Archanjo <felipecunhamendes@gmail.com>
2023-05-25 21:12:48 -0300

    CLIENT: Criação do user.api

---
+++ client/src/api/modules/user.api.js
@@ -0,0 +1,58 @@
+import privateClient from '../client/private.client';
+import publicClient from '../client/public.client';
+
+const userEndpoints = {
+	signin: '/user/signin',
+	signup: '/user/signup',
+	getUsers: ({ search }) => `/user?search=${search}`,
+}
+
+const userApi = {
+    signup: async ({name, email, password, confirmPassword, pic}) => {
+        try {
+            const response = await publicClient.post(
+                userEndpoints.signin, {
+                    name,
+                    email,
+                    password,
+                    confirmPassword,
+                    pic
+                }
+            );
+            return { response };
+        } catch (error) {
+            console.log(error);
+            return { error };
+        }
+    },
+
+    signin: async ({ email, password }) => {
+        try {
+            const response = await publicClient.post(
+                userEndpoints.signin, {
+                    email,
+                    password
+                }
+            );
+            return { response };
+        } catch (error) {
+            console.log(error);
+            return { error };
+        }
+    },
+
+    getUsers: async ({ search }) => {
+        try {
+            const response = await privateClient.get(
+                userEndpoints.getUsers({ search })
+            );
+            return { response };
+        } catch (error) {
+            console.log(error);
+            return { error };
+        }
+    }
+}
+
+
+export default userApi;
