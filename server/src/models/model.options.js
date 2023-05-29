648e5d9942e5b7ae5c9793f858e794ab29605c68
Felipe Archanjo <felipecunhamendes@gmail.com>
2023-05-29 20:07:43 -0300

    README: Explicações

--- server/src/models/model.options.js
+++ server/src/models/model.options.js
@@ -1,18 +1,4 @@
 const modelOptions = {
-	/* toJSON: {
-		virtuals: true,
-		transform: (_, obj) => {
-			delete obj._id;
-			return obj;
-		}
-	},
-	toObject: {
-		virtuals: true,
-		transform: (_, obj) => {
-			delete obj._id;
-			return obj;
-		}
-	}, */
 	versionKey: false,
 	timestamps: true
 }
